48e3c70b0d8837a1f1ce5b2465f888391f2b9e55
arhik <kkatipally@umassd.edu>
2015-06-28 16:47:14 -0400

    Weibull CDF and PDF functions

--- Monte_carlo_simulation_PDEP_relay.py
+++ Monte_carlo_simulation_PDEP_relay.py
@@ -6,8 +6,22 @@ import numpy as np
 # Computers are good at generating Uniform distribution of numbers
 # So we prefer to transform Uniform distribution to Weibull distribution
 
+# webull Random number generator
 def wblg(b,a):
     x = (1./float(b))*((-math.log(np.random.uniform()))**(1./a))
     return x
 
-    
+# Weibull PDF 
+
+def weib(x,n,a):
+    return (a * n) * (x*n)**(a - 1) * np.exp(-(x*n)**a)
+
+
+# Weibull CDF
+
+def qweib(x,n,a):
+    return (1-np.exp(-(x*n)**a))
+
+
+
+
